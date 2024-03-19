f1fd92d017fe33273ec51db86eeae5f3c81ee9c4
Hari <159746146+RHari04@users.noreply.github.com>
2024-03-19 23:21:08 +0530

    Update Arthmetic.java

--- Arthmetic.java
+++ Arthmetic.java
@@ -17,3 +17,22 @@ class Addition {
 		System.out.println("The sum = " + sum);
 	}
 }
+// Java code to illustrate Subtraction operator
+
+import java.io.*;
+
+class Subtraction {
+	public static void main(String[] args)
+	{
+		// initializing variables
+		int num1 = 20, num2 = 10, sub = 0;
+
+		// Displaying num1 and num2
+		System.out.println("num1 = " + num1);
+		System.out.println("num2 = " + num2);
+
+		// subtracting num1 and num2
+		sub = num1 - num2;
+		System.out.println("Subtraction = " + sub);
+	}
+}
